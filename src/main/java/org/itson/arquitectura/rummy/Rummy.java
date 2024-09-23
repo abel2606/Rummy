c29fd5e947971966bf00685a4b25b8c284fff9ca
abel2606 <abel.sanchez.guerrero@gmail.com>
2024-09-22 18:50:24 -0700

    Initial commit

---
+++ src/main/java/org/itson/arquitectura/rummy/Rummy.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.itson.arquitectura.rummy;
+
+/**
+ *
+ * @author Abe
+ */
+public class Rummy {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
